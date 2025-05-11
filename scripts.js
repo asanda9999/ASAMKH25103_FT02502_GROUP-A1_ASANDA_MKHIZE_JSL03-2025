85d1b707969b912b4d035e50111abdf6c009b371
asanda9999 <asandamkhize9@gmail.com>
2025-05-11 11:43:32 +0200

    Added commentting to JS

--- scripts.js
+++ scripts.js
@@ -26,18 +26,18 @@ function addNewTasks() {
   let addTask = 0;
   const maxTasks = 3;
 
-  
+  //While loop that checks task added dont exceed maxTasks  and while continue to ask user to add new tasks using confim 
   while (addTask < maxTasks) {
     const wantsToAdd = confirm("Add a new task?");
     if (!wantsToAdd) {
       break;
     }
 
-  
+  //Prompt user for task details
     const title = prompt("Enter the task title:");
     const description = prompt("Enter the task description:");
     const status = prompt("Enter the task status (todo, doing, or done):");
-
+  // Variable to store new added task details
     const newTask = {
       id: tasks.length + 1,
       title: title,
@@ -45,26 +45,28 @@ function addNewTasks() {
       status: status.toLowerCase()
     };
 
- 
+ // Push statement to add new task to the tasks array 
     tasks.push(newTask);
     addTask++;
   }
-
+  // If the user has reached the maximum number of tasks, alert them
   if (addTask === maxTasks) {
     alert("You have reached the maximum number of tasks you can add.");
   }
 }
 
-
+// Function to get all tasks with status "done"
 function getDoneTasks() {
   return tasks.filter(task => task.status === "done");
 }
 
+// windows.onload loads HTML&Css then starts the addNewTasks function
 window.onload = function () {
     addNewTasks();
   
+    // Display all tasks in the console
     console.log("All tasks:", tasks);
   
-    
+    // Display tasks with status "done" in the console
     console.log("Completed tasks:", getDoneTasks());
   };
